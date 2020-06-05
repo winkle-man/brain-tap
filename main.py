f3a6506138704e302affd77c914b8ebc320b4805
winkle-man <58744105+winkle-man@users.noreply.github.com>
2020-06-05 16:26:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,36 +2,59 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 root = Tk()
-root.title("Image Resizer")
+root.title("Image Editor")
 
-# Content
-pic_src = Image.open("fish.jpg").resize((100, 100))
-pic_conv = ImageTk.PhotoImage(pic_src)
+def resize(something):
+    global user_width, user_height, pic_width, pic_height, pic_src, pic_opened, pic_resized, pic_conv
+    pic_opened.close()
+    pic_opened = Image.open("fish.jpg")
+
+    if user_width.get() == '':
+        pic_width = 1
+    elif user_height.get() == '':
+        pic_height = 1
+    else:
+        pic_width = int(user_width.get())
+        pic_height = int(user_height.get())
+    
+    pic_resized = pic_opened.resize((pic_width, pic_height))
+    pic_conv = ImageTk.PhotoImage(pic_resized)
+    pic = Label(image = pic_conv)
+    pic.grid(row = 0, column = 1)
+
+
+
+# Contents
+pic_src = "fish.jpg"
+pic_width = 200
+pic_height = 200
+pic_opened = Image.open(pic_src)
+pic_resized = pic_opened.resize((pic_width, pic_height))
+pic_conv = ImageTk.PhotoImage(pic_resized)
+
+pic = Label(image = pic_conv)
 frame = Frame(root)
-
-height = Label(frame, text = "Height:", width = 7)
-height_inp = Entry(frame, width = 10)
-pic = Label(root, image = pic_conv)
-
-width = Label(frame, text = "Width:", width = 7)
-width_inp = Entry(frame, width = 10)
-
+label_width = Label(frame, text = "Width:", width = 7)
+user_width = Entry(frame, width = 10)
+user_width.bind("<Return>", resize)
+label_height = Label(frame, text = "Height:", width = 7)
+user_height = Entry(frame, width = 10)
+user_height.bind("<Return>", resize)
 preserve_ratio = Checkbutton(frame, text = "Preserve ratio")
-save_as = Button(frame, text = "Save as")
+btn = Button(frame, text = "Resize", command = lambda: resize(user_width, user_height))
 
-# Grid
+# Framed objects
 frame.grid(sticky = N, pady = 20)
-
-height.grid(row = 0, column = 0, sticky = E)
-width.grid(row = 1, column = 0, sticky = E)
-
-height_inp.grid(row = 0, column = 1, sticky = W)
-width_inp.grid(row = 1, column = 1, sticky = W)
-
+label_width.grid(row = 0, column = 0, sticky = E)
+user_width.grid(row = 0, column = 1, sticky = W)
+label_height.grid(row = 1, column = 0, sticky = E)
+user_height.grid(row = 1, column = 1, sticky = W)
+btn.grid(row = 2, column = 1, columnspan = 2, sticky = N, pady = 10)
 preserve_ratio.grid(row = 2, column = 0, sticky = E)
-save_as.grid(row = 2, column = 1, columnspan = 2, sticky = N, pady = 10)
 
-pic.grid(row = 0, column = 2, rowspan = 2, columnspan = 2, padx = 10, pady = 10)
+# Picture Grid
+pic.grid(row = 0, column = 1, padx = 10)
+
 
 # Main Loop
 root.mainloop()
